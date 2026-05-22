207b4dfdb0e7ac11900c153508d117102011e5cc
Kahen T <bl0ckthings@proton.me>
2026-05-22 11:32:36 +0200

    UPDATE: improved UI

--- src/app/features/applications/pages/applications-board-page/applications-board-page.ts
+++ src/app/features/applications/pages/applications-board-page/applications-board-page.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ApplicationsHeader } from '../../components/applications-header/applications-header';
+import { SortFilterBar } from '../../components/sort-filter-bar/sort-filter-bar';
+import { Board } from '../../components/board/board';
+import { ApplicationCardMenu } from '../../components/application-card-menu/application-card-menu';
+import { ApplicationCard } from '../../components/application-card/application-card';
+import { StatusColumn } from '../../components/status-column/status-column';
 
 @Component({
   selector: 'app-applications-board-page',
-  imports: [],
+  imports: [ApplicationsHeader,SortFilterBar, Board, StatusColumn,ApplicationCardMenu, ApplicationCard ],
   templateUrl: './applications-board-page.html',
   styleUrl: './applications-board-page.css',
 })
